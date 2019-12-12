26be1c617213f50122b889d89269e88482017ed6
Hui <jeffrey.x.hui@gmail.com>
2019-12-12 13:50:41 -0800

    basic app components set up, fetch is receiving data from api

---
+++ react-redux-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Components
+import Header from './components/Header'
+import Form from './components/Form'
+// Style
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
